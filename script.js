f946fa0bd4b97ae66f8c8c68a6e80575ffdd77f2
Janwin <malinaojanwin2005@gmail.com>
2025-10-29 18:00:27 +0800

    Initialize

---
+++ script.js
@@ -0,0 +1,12 @@
+const introduction = window.document.querySelector(".introduction");
+const introductionText =
+  "Hi I'm Phoenix, an Identity V pro player from Philippines, welcome to my piloting service!";
+
+let count = 0;
+setTimeout(function typing() {
+  if (count < introductionText.length) {
+    introduction.textContent += introductionText[count];
+    count++;
+    setTimeout(typing, 80);
+  }
+}, 80);
