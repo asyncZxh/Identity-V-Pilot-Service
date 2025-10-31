492d4744499b8711291c9791aa86fb62fbf252b8
Janwin <malinaojanwin2005@gmail.com>
2025-10-31 16:04:45 +0800

    Improve typing animation

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const introduction = window.document.querySelector(".introduction");
 const introductionText =
-  "Hi I'm Phoenix, an Identity V pro player from Philippines, welcome to my piloting service!";
+  "Hi I'm PHOENIX, an Identity V pro player from Philippines, welcome to my piloting service!";
 
 let count = 0;
 setTimeout(function typing() {
   if (count < introductionText.length) {
-    introduction.textContent += introductionText[count];
-    count++;
-    if (introductionText[count] === ",") setTimeout(typing, 700);
-    else setTimeout(typing, 80);
+    if (count >= 7 && count <= 13) {
+      const span = window.document.createElement("span");
+      span.textContent = introductionText[count];
+      span.style.color = "red";
+      introduction.append(span);
+      // phoenix++;
+      count++;
+      if (introductionText[count] === ",") setTimeout(typing, 700);
+      else setTimeout(typing, 80);
+    } else {
+      const span = `<span>${introductionText[count]}</span>`;
+      introduction.innerHTML += span;
+      // phoenix++;
+      count++;
+      if (introductionText[count] === ",") setTimeout(typing, 700);
+      else setTimeout(typing, 80);
+    }
   }
 }, 80);
 
@@ -32,8 +45,6 @@ const getSubTierToSurvivor = window.document.querySelector(
 const getStarsToSurvivor = window.document.querySelector(
   ".select-survivor .star-container .select-to"
 );
-
-if (getTierFromSurvivor.value === "tier-2") console.log("hound");
 
 //HUNTER
 const getTierFromHunter = window.document.querySelector(
