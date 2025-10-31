2f7827812cd4939880be56047da339fc8a7bee85
Janwin <malinaojanwin2005@gmail.com>
2025-10-31 14:35:28 +0800

    get select elements

--- script.js
+++ script.js
@@ -11,3 +11,47 @@ setTimeout(function typing() {
     else setTimeout(typing, 80);
   }
 }, 80);
+
+//SURVIVOR
+const getTierFromSurvivor = window.document.querySelector(
+  ".select-survivor .tier-container .select-from"
+);
+const getSubTierFromSurvivor = window.document.querySelector(
+  ".select-survivor .sub-tier-container .select-from"
+);
+const getStarsFromSurvivor = window.document.querySelector(
+  ".select-survivor .star-container .select-from"
+);
+
+const getTierToSurvivor = window.document.querySelector(
+  ".select-survivor .tier-container .select-to"
+);
+const getSubTierToSurvivor = window.document.querySelector(
+  ".select-survivor .sub-tier-container .select-to"
+);
+const getStarsToSurvivor = window.document.querySelector(
+  ".select-survivor .star-container .select-to"
+);
+
+if (getTierFromSurvivor.value === "tier-2") console.log("hound");
+
+//HUNTER
+const getTierFromHunter = window.document.querySelector(
+  ".select-hunter .tier-container .select-from"
+);
+const getSubTierFromHunter = window.document.querySelector(
+  ".select-hunter .sub-tier-container .select-from"
+);
+const getStarsFromHunter = window.document.querySelector(
+  ".select-hunter .star-container .select-from"
+);
+
+const getTierToHunter = window.document.querySelector(
+  ".select-hunter .tier-container .select-to"
+);
+const getSubTierToHunter = window.document.querySelector(
+  ".select-hunter .sub-tier-container .select-to"
+);
+const getStarsToHunter = window.document.querySelector(
+  ".select-hunter .star-container .select-to"
+);
