2b2aa4d136c7fa1fcae021e165f45500a4080854
Janwin <malinaojanwin2005@gmail.com>
2025-11-03 05:52:53 +0800

    add check tier

--- js/survivor.js
+++ js/survivor.js
@@ -19,7 +19,16 @@ const getStarsToSurvivor = window.document.querySelector(
 const buttonCalculate = window.document.querySelector(
   ".select-survivor .calculate-currency-container .total-button"
 );
-
+const ranks = [
+  "Bee",
+  "Hound",
+  "Elk",
+  "Mammoth",
+  "Griffin",
+  "Alicorn",
+  "Champion",
+  "Titan(Hercules)",
+];
 const prices = [
   { tier: "tier 1", price: 14 },
   { tier: "tier 2", price: 20 },
@@ -31,11 +40,29 @@ const prices = [
   { tier: "tier 8", price: 100 },
 ];
 
-getTierFromSurvivor.addEventListener("change", () => {});
+getTierFromSurvivor.addEventListener("change", () => {
+  checkTier();
+});
 
 function checkTier() {
   switch (getTierFromSurvivor.value) {
-    case "tier-1":
+    case "tier-2":
+      getTierToSurvivor.innerHTML = "";
+      let elements = "<option hidden disabled selected value>&mdash;</option>";
+      for (let i = 2; i <= 8; i++) {
+        if (i == 8) {
+          const createElement = `<option value="peak-tier">Peak Tier - ${
+            ranks[ranks.length - 1]
+          }}</option>`;
+          elements += createElement;
+        } else {
+          const createElement = `<option value="tier-${i}">Tier ${i} - ${
+            ranks[i - 1]
+          }</option>`;
+          elements += createElement;
+        }
+      }
+      getTierToSurvivor.innerHTML = elements;
       break;
   }
 }
