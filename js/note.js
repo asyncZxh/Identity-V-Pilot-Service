f22e8c8126dd5368f0e5d442ef88011cb43d88d5
Janwin <malinaojanwin2005@gmail.com>
2025-11-16 08:55:40 +0800

    change note content + add griffin

--- js/note.js
+++ js/note.js
@@ -6,6 +6,7 @@ import {
   getSubTierToSurvivor,
   getStarsToSurvivor,
   buttonCalculateSurvivor,
+  currencySelectionSurvivor,
 } from "./survivor.js";
 
 import {
@@ -16,6 +17,7 @@ import {
   getSubTierToHunter,
   getStarsToHunter,
   buttonCalculateHunter,
+  currencySelectionHunter,
 } from "./hunter.js";
 
 const openNoteBtnContainer = window.document.querySelector(
@@ -24,16 +26,18 @@ const openNoteBtnContainer = window.document.querySelector(
 const openNoteBtn = window.document.querySelector(".note-btn");
 const closeNoteBtn = window.document.querySelector(".close-btn");
 const note = window.document.querySelector(".note");
-const noteContent = `<li>Bee 3 - 3 stars = Bee 2 - 0 star</li><li>Bee 2 - 3 stars = Bee 1 - 0 star</li><li>Bee 1 - 3 stars = Hound 4 - 0 star</li>
-                    <li>Hound 4 - 4 stars = Hound 3 - 0 star</li><li>Hound 3 - 4 stars = Hound 2 - 0 star</li><li>Hound 2 - 4 stars = Hound 1 - 0 star</li><li>Hound 1 - 4 stars = Elk 5 - 0 star</li>
-                    <li>Elk 5 - 5 stars = Elk 4 - 0 star</li><li>Elk 4 - 5 stars = Elk 3 - 0 star</li><li>Elk 3 - 5 stars = Elk 2 - 0 star</li><li>Elk 2 - 5 stars = Elk 1 - 0 star</li><li>Elk 1 - 5 stars = Mammoth 5 - 0 star</li>
-                    <li>Mammoth 5 - 5 stars = Mammoth 4 - 0 star</li><li>Mammoth 4 - 5 stars = Mammoth 3 - 0 star</li><li>Mammoth 3 - 5 stars = Mammoth 2 - 0 star</li><li>Mammoth 2 - 5 stars = Mammoth 1 - 0 star</li><li>Mammoth 1 - 5 stars = Alicorn 5 - 0 star</li>
-                    <li>Alicorn 5 - 5 stars = Alicorn 4 - 0 star</li><li>Alicorn 4 - 5 stars = Alicorn 3 - 0 star</li><li>Alicorn 3 - 5 stars = Alicorn 2 - 0 star</li><li>Alicorn 2 - 5 stars = Alicorn 1 - 0 star</li><li>Alicorn 1 - 5 stars = Champion - 0 star</li>
-                    <li>Spider 3 - 3 stars = Spider 2 - 0 star</li><li>Spider 2 - 3 stars = Spider 1 - 0 star</li><li>Spider 1 - 3 stars = Cobra 4 - 0 star</li>
-                    <li>Cobra 4 - 4 stars = Cobra 3 - 0 star</li><li>Cobra 3 - 4 stars = Cobra 2 - 0 star</li><li>Cobra 2 - 4 stars = Cobra 1 - 0 star</li><li>Cobra 1 - 4 stars = Crocodile 5 - 0 star</li>
-                    <li>Crocodile 5 - 5 stars = Crocodile 4 - 0 star</li><li>Crocodile 4 - 5 stars = Crocodile 3 - 0 star</li><li>Crocodile 3 - 5 stars = Crocodile 2 - 0 star</li><li>Crocodile 2 - 5 stars = Crocodile 1 - 0 star</li><li>Crocodile 1 - 5 stars = Sabertooth 5 - 0 star</li>
-                    <li>Sabertooth 5 - 5 stars = Sabertooth 4 - 0 star</li><li>Sabertooth 4 - 5 stars = Sabertooth 3 - 0 star</li><li>Sabertooth 3 - 5 stars = Sabertooth 2 - 0 star</li><li>Sabertooth 2 - 5 stars = Sabertooth 1 - 0 star</li><li>Sabertooth 1 - 5 stars = Cyclops 5 - 0 star</li>
-                    <li>Cyclops 5 - 5 stars = Cyclops 4 - 0 star</li><li>Cyclops 4 - 5 stars = Cyclops 3 - 0 star</li><li>Cyclops 3 - 5 stars = Cyclops 2 - 0 star</li><li>Cyclops 2 - 5 stars = Cyclops 1 - 0 star</li><li>Cyclops 1 - 5 stars = Evil Dragon - 0 star</li>`;
+const noteContent = `<li>Bee 3 - 3 star = Bee 2 - 0 star</li><li>Bee 2 - 3 star = Bee 1 - 0 star</li><li>Bee 1 - 3 star = Hound 4 - 0 star</li>
+                    <li>Hound 4 - 4 star = Hound 3 - 0 star</li><li>Hound 3 - 4 star = Hound 2 - 0 star</li><li>Hound 2 - 4 star = Hound 1 - 0 star</li><li>Hound 1 - 4 star = Elk 5 - 0 star</li>
+                    <li>Elk 5 - 5 star = Elk 4 - 0 star</li><li>Elk 4 - 5 star = Elk 3 - 0 star</li><li>Elk 3 - 5 star = Elk 2 - 0 star</li><li>Elk 2 - 5 star = Elk 1 - 0 star</li><li>Elk 1 - 5 star = Mammoth 5 - 0 star</li>
+                    <li>Mammoth 5 - 5 star = Mammoth 4 - 0 star</li><li>Mammoth 4 - 5 star = Mammoth 3 - 0 star</li><li>Mammoth 3 - 5 star = Mammoth 2 - 0 star</li><li>Mammoth 2 - 5 star = Mammoth 1 - 0 star</li><li>Mammoth 1 - 5 star = Griffin 5 - 0 star</li>
+                    <li>Griffin 5 - 5 star = Griffin 4 - 0 star</li><li>Griffin 4 - 5 star = Griffin 3 - 0 star</li><li>Griffin 3 - 5 star = Griffin 2 - 0 star</li><li>Griffin 2 - 5 star = Griffin 1 - 0 star</li><li>Griffin 1 - 5 star = Alicorn 5 - 0 star</li>
+                    <li>Alicorn 5 - 5 star = Alicorn 4 - 0 star</li><li>Alicorn 4 - 5 star = Alicorn 3 - 0 star</li><li>Alicorn 3 - 5 star = Alicorn 2 - 0 star</li><li>Alicorn 2 - 5 star = Alicorn 1 - 0 star</li><li>Alicorn 1 - 5 star = Champion - 0 star</li>
+                    <li>Spider 3 - 3 star = Spider 2 - 0 star</li><li>Spider 2 - 3 star = Spider 1 - 0 star</li><li>Spider 1 - 3 star = Cobra 4 - 0 star</li>
+                    <li>Cobra 4 - 4 star = Cobra 3 - 0 star</li><li>Cobra 3 - 4 star = Cobra 2 - 0 star</li><li>Cobra 2 - 4 star = Cobra 1 - 0 star</li><li>Cobra 1 - 4 star = Crocodile 5 - 0 star</li>
+                    <li>Crocodile 5 - 5 star = Crocodile 4 - 0 star</li><li>Crocodile 4 - 5 star = Crocodile 3 - 0 star</li><li>Crocodile 3 - 5 star = Crocodile 2 - 0 star</li><li>Crocodile 2 - 5 star = Crocodile 1 - 0 star</li><li>Crocodile 1 - 5 star = Sabertooth 5 - 0 star</li>
+                    <li>Sabertooth 5 - 5 star = Sabertooth 4 - 0 star</li><li>Sabertooth 4 - 5 star = Sabertooth 3 - 0 star</li><li>Sabertooth 3 - 5 star = Sabertooth 2 - 0 star</li><li>Sabertooth 2 - 5 star = Sabertooth 1 - 0 star</li><li>Sabertooth 1 - 5 star = Manticore 5 - 0 star</li>
+                    <li>Manticore 5 - 5 star = Manticore 4 - 0 star</li><li>Manticore 4 - 5 star = Manticore 3 - 0 star</li><li>Manticore 3 - 5 star = Manticore 2 - 0 star</li><li>Manticore 2 - 5 star = Manticore 1 - 0 star</li><li>Manticore 1 - 5 star = Cyclops 5 - 0 star</li>
+                    <li>Cyclops 5 - 5 star = Cyclops 4 - 0 star</li><li>Cyclops 4 - 5 star = Cyclops 3 - 0 star</li><li>Cyclops 3 - 5 star = Cyclops 2 - 0 star</li><li>Cyclops 2 - 5 star = Cyclops 1 - 0 star</li><li>Cyclops 1 - 5 star = Evil Dragon - 0 star</li>`;
 const noteList = window.document.querySelector(".note-list");
 const inputs = [
   getTierFromSurvivor,
@@ -43,6 +47,7 @@ const inputs = [
   getSubTierToSurvivor,
   getStarsToSurvivor,
   buttonCalculateSurvivor,
+  currencySelectionSurvivor,
   getTierFromHunter,
   getSubTierFromHunter,
   getStarsFromHunter,
@@ -50,6 +55,7 @@ const inputs = [
   getSubTierToHunter,
   getStarsToHunter,
   buttonCalculateHunter,
+  currencySelectionHunter,
 ];
 
 noteList.innerHTML = noteContent;
@@ -61,10 +67,12 @@ openNoteBtn.addEventListener("click", () => {
     note.classList.add("open-note");
     openNoteBtnContainer.style.zIndex = 0;
     note.style.zIndex = 1;
-    inputs.forEach((e) => {
+    inputs.forEach((e, i) => {
       e.tabindex = "-1";
       e.style.pointerEvents = "none";
       e.setAttribute("disabled", "true");
+      if (i === inputs.length - 1 / 2 + 0.5 || i === inputs.length - 1)
+        e.style.color = "red";
     });
     setTimeout(() => (isOpen = false), 500);
   }
