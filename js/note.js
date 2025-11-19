a3e811b62916ce68250335a60009c8459beeae42
Janwin <malinaojanwin2005@gmail.com>
2025-11-19 08:17:36 +0800

    note list modified

--- js/note.js
+++ js/note.js
@@ -163,11 +163,11 @@ function refreshSubTierEl(el1, el2, el3, el4) {
 
 function renderNoteList() {
   let noteContent = "";
-  function renderTier(tier, nextTier, subtier) {
+  function renderTier(tier, nextTier, subTier, nextSubTier, star) {
     {
-      for (let s = subtier; s >= 1; s--) {
-        if (s === 1) {
-          noteContent += `<li>${tier} ${s} - 5 <svg
+      for (subTier; subTier >= 1; subTier--) {
+        if (subTier === 1) {
+          noteContent += `<li>${tier} ${subTier} - ${star} <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -180,7 +180,7 @@ function renderNoteList() {
                     stroke-linejoin="round"
                     d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
                   />
-                </svg> &nbsp; &#61; &nbsp; ${nextTier} ${s + 4} - 0 <svg
+                </svg> &nbsp; &#61; &nbsp; ${nextTier} ${nextSubTier} - 0 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -196,7 +196,7 @@ function renderNoteList() {
                 </svg>`;
           break;
         }
-        noteContent += `<li>${tier} ${s} - 5 <svg
+        noteContent += `<li>${tier} ${subTier} - ${star} <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -209,7 +209,7 @@ function renderNoteList() {
                     stroke-linejoin="round"
                     d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
                   />
-                </svg> &nbsp; &#61; &nbsp; ${tier} ${s - 1} - 0 <svg
+                </svg> &nbsp; &#61; &nbsp; ${tier} ${subTier - 1} - 0 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -228,20 +228,20 @@ function renderNoteList() {
   }
 
   //render tier(survivor)
-  renderTier("Bee", "Hound", 3);
-  renderTier("Hound", "Elk", 4);
-  renderTier("Elk", "Mammoth", 5);
-  renderTier("Mammoth", "Griffin", 5);
-  renderTier("Griffin", "Alicorn", 5);
-  renderTier("Alicorn", "Champion", 5);
+  renderTier("Bee", "Hound", 3, 4, 3);
+  renderTier("Hound", "Elk", 4, 5, 4);
+  renderTier("Elk", "Mammoth", 5, 5, 5);
+  renderTier("Mammoth", "Griffin", 5, 5, 5);
+  renderTier("Griffin", "Alicorn", 5, 5, 5);
+  renderTier("Alicorn", "Champion", 5, "", 5);
 
   //render tier(hunter)
-  renderTier("Spider", "Cobra", 3);
-  renderTier("Cobra", "Crocodile", 4);
-  renderTier("Crocodile", "Sabertooth", 5);
-  renderTier("Sabertooth", "Manticore", 5);
-  renderTier("Manticore", "Cyclops", 5);
-  renderTier("Cyclops", "Evil Dragon", 5);
+  renderTier("Spider", "Cobra", 3, 4, 3);
+  renderTier("Cobra", "Crocodile", 4, 5, 4);
+  renderTier("Crocodile", "Sabertooth", 5, 5, 5);
+  renderTier("Sabertooth", "Manticore", 5, 5, 5);
+  renderTier("Manticore", "Cyclops", 5, 5, 5);
+  renderTier("Cyclops", "Evil Dragon", 5, "", 5);
 
   noteList.innerHTML = noteContent;
 }
