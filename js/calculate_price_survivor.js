484c243df4919844c7a8306faf6858537871371b
Janwin <malinaojanwin2005@gmail.com>
2025-11-22 18:39:19 +0800

    fix sub-tier index

--- js/calculate_price_survivor.js
+++ js/calculate_price_survivor.js
@@ -77,14 +77,12 @@ buttonCalculateSurvivor.addEventListener("click", () => {
 });
 
 function getPrice() {
-  const tierFrom = parseInt(getTierFromSurvivor.value) || undefined;
-  const subTierFrom = parseInt(getSubTierFromSurvivor.value) || undefined;
-  if (getTierFromSurvivor.value === "8") {
-  }
-  const starFrom = parseInt(getStarsFromSurvivor.value);
-  const tierTo = parseInt(getTierToSurvivor.value) | undefined;
-  const subTierTo = parseInt(getSubTierToSurvivor.value) || undefined;
-  const starTo = parseInt(getStarsToSurvivor.value);
+  let tierFrom = parseInt(getTierFromSurvivor.value) || undefined;
+  let subTierFrom = parseInt(getSubTierFromSurvivor.value) || undefined;
+  let starFrom = parseInt(getStarsFromSurvivor.value);
+  let tierTo = parseInt(getTierToSurvivor.value) || undefined;
+  let subTierTo = parseInt(getSubTierToSurvivor.value) || undefined;
+  let starTo = parseInt(getStarsToSurvivor.value);
 
   if (getTierFromSurvivor.value !== "8" && getTierToSurvivor.value === "8") {
     if (parseInt(getStarsToSurvivor.value) < 25) {
@@ -104,6 +102,7 @@ function getPrice() {
       !starTo
     ) {
       window.alert("Missing required fields");
+      console.log("test1");
       return undefined;
     }
   } else if (
@@ -113,6 +112,7 @@ function getPrice() {
   ) {
     if (!tierFrom || !subTierFrom || !starFrom || !tierTo || !starTo) {
       window.alert("Missing required fields");
+      console.log("test2");
       return undefined;
     }
   } else if (
@@ -121,6 +121,7 @@ function getPrice() {
   ) {
     if (!tierFrom || !starFrom || !tierTo || !starTo) {
       window.alert("Missing required fields");
+      console.log("test3");
       return undefined;
     } else if (parseInt(getStarsFromSurvivor.value) < 25) {
       window.alert("Invalid star input");
@@ -136,26 +137,125 @@ function getPrice() {
       alert("Tier: TITAN\nFROM must not be equal to TO");
       return undefined;
     }
-    window.console.log("eee");
   } else if (
     getTierFromSurvivor.value === "7" ||
     getTierFromSurvivor.value === "8"
   ) {
     if (!tierFrom || !starFrom || !tierTo || !starTo) {
       window.alert("Missing required fields");
+      console.log("test4");
       return undefined;
     }
   }
 
+  const modifySubTier = () => {
+    if (tierFrom === 1) {
+      switch (subTierFrom) {
+        case 3:
+          subTierFrom = 0;
+          break;
+        case 2:
+          subTierFrom = 1;
+          break;
+        case 1:
+          subTierFrom = 2;
+          break;
+      }
+    } else if (tierFrom === 2) {
+      switch (subTierFrom) {
+        case 4:
+          subTierFrom = 0;
+          break;
+        case 3:
+          subTierFrom = 1;
+          break;
+        case 2:
+          subTierFrom = 2;
+          break;
+        case 1:
+          subTierFrom = 3;
+          break;
+      }
+    } else if (tierFrom !== 7 && tierFrom !== 8) {
+      switch (subTierFrom) {
+        case 5:
+          subTierFrom = 0;
+          break;
+        case 4:
+          subTierFrom = 1;
+          break;
+        case 3:
+          subTierFrom = 2;
+          break;
+        case 2:
+          subTierFrom = 3;
+          break;
+        case 1:
+          subTierFrom = 4;
+          break;
+      }
+    }
+
+    if (tierTo === 1) {
+      switch (subTierTo) {
+        case 3:
+          subTierTo = 0;
+          break;
+        case 2:
+          subTierTo = 1;
+          break;
+        case 1:
+          subTierTo = 2;
+          break;
+      }
+    } else if (tierTo === 2) {
+      switch (subTierTo) {
+        case 4:
+          subTierTo = 0;
+          break;
+        case 3:
+          subTierTo = 1;
+          break;
+        case 2:
+          subTierTo = 2;
+          break;
+        case 1:
+          subTierTo = 3;
+          break;
+      }
+    } else if (tierTo !== 7 && tierTo !== 8) {
+      switch (subTierTo) {
+        case 5:
+          subTierTo = 0;
+          break;
+        case 4:
+          subTierTo = 1;
+          break;
+        case 3:
+          subTierTo = 2;
+          break;
+        case 2:
+          subTierTo = 3;
+          break;
+        case 1:
+          subTierTo = 4;
+          break;
+      }
+    }
+  };
+  modifySubTier();
+  tierFrom--;
+  tierTo--;
+
   return {
     From: {
-      Tier: tierFrom - 1,
-      "Sub-tier": subTierFrom - 1,
+      Tier: tierFrom,
+      "Sub-tier": subTierFrom,
       Star: starFrom,
     },
     To: {
-      Tier: tierTo - 1,
-      "Sub-tier": subTierTo - 1,
+      Tier: tierTo,
+      "Sub-tier": subTierTo,
       Star: starTo,
     },
   };
