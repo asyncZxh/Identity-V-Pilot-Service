39da72c3c7a212bb4ebe2bafcfb274ddd0f42962
Janwin <malinaojanwin2005@gmail.com>
2025-12-02 13:46:33 +0800

    add js for converting currency

---
+++ js/convert_currency.js
@@ -0,0 +1,6 @@
+const currencySelectionSurvivorSelect = window.document.querySelector(
+  ".select-survivor .currency-container .currency.convert"
+);
+const currencySelectionHunterSelect = window.document.querySelector(
+  ".select-hunter .currency-container .currency.convert"
+);
