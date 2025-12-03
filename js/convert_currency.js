d7924fe95d9aa351ce8e1fd5bd541e0097b67e4a
Janwin <malinaojanwin2005@gmail.com>
2025-12-03 08:32:23 +0800

    clear price container when changing currency

--- js/convert_currency.js
+++ js/convert_currency.js
@@ -4,20 +4,23 @@ const currencySelectionSurvivor = window.document.querySelector(
 const currencySelectionHunter = window.document.querySelector(
   ".select-hunter .currency-container .currency.convert"
 );
-
 const currencyTextSurvivor = window.document.querySelector(
   ".select-survivor .currency-container .currency-text"
 );
-
 const currencyTextHunter = window.document.querySelector(
   ".select-hunter .currency-container .currency-text"
 );
-
-currencySelectionSurvivor.addEventListener(
-  "change",
-  () => (currencyTextSurvivor.textContent = currencySelectionSurvivor.value)
+const totalPriceContainerSurvivor = window.document.querySelector(
+  ".total-price-survivor"
 );
-currencySelectionHunter.addEventListener(
-  "change",
-  () => (currencyTextHunter.textContent = currencySelectionHunter.value)
+const totalPriceContainerHunter = window.document.querySelector(
+  ".total-price-hunter"
 );
+currencySelectionSurvivor.addEventListener("change", () => {
+  currencyTextSurvivor.textContent = currencySelectionSurvivor.value;
+  totalPriceContainerSurvivor.innerHTML = "";
+});
+currencySelectionHunter.addEventListener("change", () => {
+  currencyTextHunter.textContent = currencySelectionHunter.value;
+  totalPriceContainerHunter.innerHTML = "";
+});
