ef06825df97ab4a2e4da25bdac02e33a47a8072b
Janwin <malinaojanwin2005@gmail.com>
2025-12-12 20:57:43 +0800

    separate data

---
+++ data/hunter_rates_data.js
@@ -0,0 +1,50 @@
+const hunterRates = [
+  {
+    tier: "Tier 1",
+    img_src: "images/tiers/new/hunter/Tarantula.webp",
+    alt: "Spider",
+    price: 12,
+  },
+  {
+    tier: "Tier 2",
+    img_src: "images/tiers/new/hunter/Cobra.webp",
+    alt: "Cobra",
+    price: 20,
+  },
+  {
+    tier: "Tier 3",
+    img_src: "images/tiers/new/hunter/Crocodile.webp",
+    alt: "Crocodile",
+    price: 30,
+  },
+  {
+    tier: "Tier 4",
+    img_src: "images/tiers/new/hunter/SaberToothedTiger.webp",
+    alt: "Sabertooth",
+    price: 40,
+  },
+  {
+    tier: "Tier 5",
+    img_src: "images/tiers/new/hunter/Manticore.webp",
+    alt: "Manticore",
+    price: 50,
+  },
+  {
+    tier: "Tier 6",
+    img_src: "images/tiers/new/hunter/OneEyedGiant.webp",
+    alt: "Cyclops",
+    price: 65,
+  },
+  {
+    tier: "Tier 7",
+    img_src: "images/tiers/new/hunter/EvilDragon.webp",
+    alt: "Evil Dragon",
+    price: 80,
+  },
+  {
+    tier: "Peak Tier",
+    img_src: "images/tiers/new/hunter/Hydra.webp",
+    alt: "Hydra - Peak Tier",
+    price: 80,
+  },
+];
---
+++ data/survivor_rates_data.js
@@ -0,0 +1,50 @@
+const survivorRates = [
+  {
+    tier: "Tier 1",
+    img_src: "images/tiers/new/survivor/WorkerBee.webp",
+    alt: "Worker Bee",
+    price: 15,
+  },
+  {
+    tier: "Tier 2",
+    img_src: "images/tiers/new/survivor/HoundTier.webp",
+    alt: "Hound",
+    price: 20,
+  },
+  {
+    tier: "Tier 3",
+    img_src: "images/tiers/new/survivor/Elk.webp",
+    alt: "Elk",
+    price: 30,
+  },
+  {
+    tier: "Tier 4",
+    img_src: "images/tiers/new/survivor/Mammoth.webp",
+    alt: "Mammoth",
+    price: 40,
+  },
+  {
+    tier: "Tier 5",
+    img_src: "images/tiers/new/survivor/Griffin.webp",
+    alt: "Griffin",
+    price: 50,
+  },
+  {
+    tier: "Tier 6",
+    img_src: "images/tiers/new/survivor/Unicorn.webp",
+    alt: "Alicorn",
+    price: 70,
+  },
+  {
+    tier: "Tier 7",
+    img_src: "images/tiers/new/survivor/ChampionTier.webp",
+    alt: "Champion",
+    price: 85,
+  },
+  {
+    tier: "Peak Tier",
+    img_src: "images/tiers/new/survivor/Titan.webp",
+    alt: "Titan - Peak Tier",
+    price: 95,
+  },
+];
