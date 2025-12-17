18129ef756629c658c9159a7941aa77e8d7b1896
Janwin <malinaojanwin2005@gmail.com>
2025-12-17 23:37:08 +0800

    remove spacing at top variables on hunter.js

--- js/hunter.js
+++ js/hunter.js
@@ -1,39 +1,30 @@
 export const getTierFromHunter = window.document.querySelector(
   ".select-hunter .tier-container .select-from"
 );
-
 export const getSubTierFromHunter = window.document.querySelector(
   ".select-hunter .sub-tier-container .select-from"
 );
-
 export let getStarsFromHunter = window.document.querySelector(
   ".select-hunter .star-container .select-from"
 );
-
 export const getTierToHunter = window.document.querySelector(
   ".select-hunter .tier-container .select-to"
 );
-
 export const getSubTierToHunter = window.document.querySelector(
   ".select-hunter .sub-tier-container .select-to"
 );
-
 export let getStarsToHunter = window.document.querySelector(
   ".select-hunter .star-container .select-to"
 );
-
 export const buttonCalculateHunter = window.document.querySelector(
   ".select-hunter .calculate-currency-container .total-button"
 );
-
 export const currencySelectionHunterSelect = window.document.querySelector(
   ".select-hunter .currency-container .currency.convert"
 );
-
 export const currencySelectionHunterIcon = window.document.querySelector(
   ".select-hunter .currency-container .chevron-down"
 );
-
 const ranks = [
   "Spider",
   "Cobra",
